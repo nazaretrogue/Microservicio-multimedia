57392281c687626248f55b83c24551a8101cccf1
Nazaret <nazaretrogue@correo.ugr.es>
2019-09-27 21:49:29 +0200

    Añadido primer test de la tarea #2

---
+++ test/aserciones.java
@@ -0,0 +1,27 @@
+package tests;
+
+import java.awt.image.BufferedImage;
+
+public class MultimediaTests {
+    public void JPGTest(BufferedImage img_source){
+        /*try{
+            File f = new File(dlg.getSelectedFile().getAbsolutePath()){
+                @Override
+                public String toString(){
+                    return this.getName();
+                }
+            };
+
+            String[] nombre_archivo = f.toString().split("\\.");
+
+            if(nombre_archivo[1].toUpperCase() == "JPEG" || nombre_archivo[1].toUpperCase() == "JPG"){
+                BufferedImage img = ImageIO.read(f);
+            }
+
+        }catch(Exception e){
+                System.err.println("Error al leer el fichero");
+        }*/
+
+        assertTrue(false);
+    }
+}
