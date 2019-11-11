269ecae320ed70e3eae25fb5ba19d60061ee7b1a
Nazaret <nazaretrogue@correo.ugr.es>
2019-11-11 12:42:58 +0100

    Eliminado fallo en app.py. #13

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import os, sys
 from src.sender import enviar
-from celery import Celery
 
 app = Flask(__name__, template_folder="templates")
 
